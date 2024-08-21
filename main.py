eb9d51cb2b12542994a42fb22ab92dd60b4a11b1
UNKNOWN9073 <raihankhan91237@gmail.com>
2024-08-21 12:42:45 +0600

    main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 
 while True:
     os.system("clear")
-    print("[1] time to Bulk sms\n[2] Github user info")
+    print("[1] time to Bulk sms\n[2] Github user info \n[3] Fb Post share ")
     a = input("[>] Select Your Option: ") 
 
     if a == "1":
@@ -11,6 +11,9 @@ while True:
     elif a == "2":
         os.system("python3 github.py")
         a=input()
+    elif a == "3":
+        os.system("python3 Fbshare.py")
+        a=input()    
     else:
         print("[x] Wrong Value Entered!")
         a=input()
